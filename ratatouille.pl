4e5d36664cbe04aaf5e4a9670d55d732c635c7a6
Lucas Spigariol <lspigariol@gmail.com>
2020-07-06 18:58:03 -0300

    Create ratatouille.pl

---
+++ ratatouille.pl
@@ -0,0 +1,18 @@
+rata(remy, gusteaus).
+rata(emile, bar).
+rata(django, pizzeria).
+
+cocina(linguini, ratatouille, 3).
+cocina(linguini, sopa, 5).
+cocina(colette, salmonAsado, 9).
+cocina(horst, ensaladaRusa, 8).
+
+trabajaEn(gusteaus, linguini).
+trabajaEn(gusteaus, colette).
+trabajaEn(gusteaus, skinner).
+trabajaEn(gusteaus, horst).
+trabajaEn(cafeDes2Moulins, amelie).
+
+plato(ensaladaRusa, entrada([papa, zanahoria, arvejas, huevo, mayonesa])).
+plato(bifeDeChorizo, principal(pure, 20)).
+plato(frutillasConCrema, postre(265)).
